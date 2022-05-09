7887cfe2c703fdedb92c1aa9cb8ac5364096c5fe
zrh-LegionLinux <11912614@mail.sustech.edu.cn>
2022-05-09 17:22:34 +0800

    update

--- src/main/java/io/github/joaomlneto/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/io/github/joaomlneto/travis_ci_tutorial_java/SimpleCalculator.java
@@ -21,6 +21,7 @@ public class SimpleCalculator {
         System.out.println("hi");
         System.out.println("hello");
         System.out.println("hello2");
+        System.out.println("hello3");
         System.out.println("Hello world?");
     }
 
